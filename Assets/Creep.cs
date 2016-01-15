fe3013252183b244e234022fb33d3b0697be3749
i3anaan <i3anaan@live.nl>
2016-01-15 17:51:19 +0100

    Initial commit

---
+++ Assets/Creep.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Creep : MonoBehaviour {
+
+	public static float BASE_SPEED = 0.03f;
+	public static float BASE_DAMAGE = 1f;
+	public static float BASE_LIFE = 100f;
+
+	private GameController gc;
+	private List<Creep> creeps;
+	public int team;
+	public float speed;
+	public float damage;
+	public float life;
+	private float[] priority = new float[]{1f, 1f, 1f, 3f};
+
+	// Use this for initialization
+	void Start () {
+		gc = GameObject.FindGameObjectWithTag ("GameController").GetComponent<GameController>();
+		creeps = gc.creeps;
+		ReScale ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Creep target = FindTarget ();
+
+		//Vector3 dir = (target.transform.position - this.transform.position).normalized;
+
+		this.transform.position = Vector3.MoveTowards (this.transform.position, target.transform.position, speed);
+
+		if (Vector3.Distance (this.transform.position, target.transform.position) < (this.transform.localScale.x + target.transform.localScale.x) * 0.6f) {
+			//Fighting the target;
+			this.Attack (target);
+		}
+	}
+
+	void TakeDamage(float damage) {
+		life -= damage;
+		ReScale ();
+
+		if (this.life < 0) {
+			GameObject.Destroy (this.gameObject);
+		}
+	}
+
+	void Attack(Creep target) {
+		target.TakeDamage(this.damage);
+	}
+
+	void ReScale() {
+		float scale = this.life / BASE_LIFE;
+		this.transform.localScale = new Vector3 (scale, scale, scale);
+	}
+
+	Creep FindTarget() {
+		float minDistance = float.MaxValue;
+		Creep target = null;
+
+		foreach (Creep c in creeps) {
+			if (c && c.team != this.team) {
+				float dist = Vector3.Distance(this.transform.position, c.gameObject.transform.position);
+				if (dist / priority[c.team] < minDistance) {
+					minDistance = dist / priority[c.team];
+					target = c;
+				}
+			}
+		}
+
+		if (!target) {
+			gc.GameOver();
+		}
+
+		return target;
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour {
+
+	public Creep creepPrefab;
+	public int teamCount;
+	public int creepCountPerTeam;
+	public List<Creep> creeps;
+	public float xRange;
+	public float yRange;
+
+	private Color[] colors = new Color[]{Color.red, Color.blue, Color.green, Color.yellow};
+
+	void Awake() {
+		for (int t = 0; t < teamCount; t++) {
+			Color color = colors [t];
+			for (int c = 0; c < creepCountPerTeam; c++) {
+				Creep newCreep = (Creep) GameObject.Instantiate (creepPrefab, new Vector3 (), Quaternion.identity);
+				newCreep.gameObject.GetComponent<SpriteRenderer>().color = color;
+				newCreep.team = t;
+				newCreep.speed = Random.Range (0.5f, 1.5f) * Creep.BASE_SPEED;
+				newCreep.damage = Random.Range (0.5f, 1.5f) * Creep.BASE_DAMAGE;
+				newCreep.life = Random.Range(0.5f, 1.5f) * Creep.BASE_LIFE;
+				creeps.Add (newCreep);
+			}
+		}
+
+		SpawnCreeps ();
+	}
+
+
+	void SpawnCreeps() {
+		foreach (Creep creep in creeps) {
+			creep.transform.position = new Vector3(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange), 0);
+		}
+	}
+
+	public void GameOver() {
+		Application.LoadLevel ("main");
+	}
+}
