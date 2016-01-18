ffade3f8ce7024c40afbd48ab01d48a0c3f00495
i3anaan <i3anaan@live.nl>
2016-01-18 22:47:17 +0100

    Restarted working on mp using low level api, can connect

---
+++ Assets/Scripts/ClientNetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ClientNetworkManager : MonoBehaviour {
+
+	private int reliableChannelId;
+	private int unreliableChannelId;
+	HostTopology topology;
+	int hostId;
+	private int connectionId;
+
+	// Use this for initialization
+	void Start () {
+		NetworkTransport.Init ();
+
+		ConnectionConfig config = new ConnectionConfig();
+		reliableChannelId  = config.AddChannel(QosType.Reliable);
+		unreliableChannelId = config.AddChannel(QosType.Unreliable);
+
+		topology = new HostTopology(config, 4);
+
+		hostId = NetworkTransport.AddHost(topology, 7788);
+
+		byte error;
+		byte[] buffer = new byte[]{1};
+		int bufferLength = 1;
+		connectionId = NetworkTransport.Connect(hostId, "130.89.183.126", 7777, 0, out error);
+		Debug.Log (connectionId);
+		NetworkTransport.Send(hostId, connectionId, reliableChannelId, buffer, bufferLength,  out error);
+		Debug.Log ("Sending data!");
+	}
+}
--- Assets/Scripts/NetworkPlayer.cs
+++ Assets/Scripts/NetworkPlayer.cs
@@ -4,7 +4,6 @@ using System.Collections;
 
 public class NetworkPlayer : NetworkBehaviour {
 
-	[SyncVar]
 	public TeamPriorities priorities;
 
 	[Command]
