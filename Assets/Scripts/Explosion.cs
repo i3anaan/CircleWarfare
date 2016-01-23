65909456dcb03ac2e3d29224a4690c0d8caa43c6
i3anaan <i3anaan@live.nl>
2016-01-23 19:03:02 +0100

    Cleaner hierarchy

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -8,11 +8,13 @@ public class Explosion : MonoBehaviour {
 	public float force;
 	public Color color;
 
-	void Start() {
+	public void Start() {
+		this.transform.SetParent(SimulationServerGameController.GetDebrisStore ().transform);
 		debrisPrefab.GetComponents<SpriteRenderer> () [0].color = color;
 		for (int i = 0; i < debrisCount; i++) {
 			Quaternion rotation = Quaternion.AngleAxis (Random.Range (0f, 360f), Vector3.forward);
 			GameObject debris = (GameObject) GameObject.Instantiate (debrisPrefab, this.transform.position, rotation);
+			debris.transform.SetParent (SimulationServerGameController.GetDebrisStore ().transform);
 			debris.GetComponent<Rigidbody2D>().AddForce(debris.transform.up * force);
 		}
 	}
--- Assets/Scripts/SimulationServerGameController.cs
+++ Assets/Scripts/SimulationServerGameController.cs
@@ -24,10 +24,26 @@ public class SimulationServerGameController : BaseServerGameController {
 	private bool started;
 	public bool firstBlood;
 
+	private static GameObject creepStore;
+	private static GameObject debrisStore;
+
 	private int ticksToRestoreTimeScale;
 
 	public static Color[] colors = new Color[]{Color.red, Color.blue, Color.green, Color.yellow, Color.cyan, Color.magenta, Color.grey};
 	//TODO move this somewhere else?
+
+	public static GameObject GetCreepStore() {
+		if (creepStore == null) {
+			creepStore = new GameObject ("creepStore");
+		}
+		return creepStore;
+	}
+	public static GameObject GetDebrisStore() {
+		if (debrisStore == null) {
+			debrisStore = new GameObject ("debrisStore");
+		}
+		return debrisStore;
+	}
 
 	public void FixedUpdate() {
 		if (Time.timeScale < 1) {
@@ -40,36 +56,34 @@ public class SimulationServerGameController : BaseServerGameController {
 	public void StartGame() {
 		if (!started) {
 			started = true;
-
-			for (int t = 0; t < networkManager.connectionIds.Count; t++) {
-				Color color = colors [t];
-				int playerId = networkManager.connectionIds [t];
-				for (int c = 0; c < creepCountPerTeam; c++) {
-					Creep newCreep = (Creep)GameObject.Instantiate (creepPrefab, new Vector3 (), Quaternion.identity);
-					ClientData client = networkManager.GetClientData (playerId);
-					newCreep.gc = this;
-					newCreep.creeps = creeps;
-					newCreep.gameObject.GetComponent<SpriteRenderer> ().color = color;
-					newCreep.team = t;
-					newCreep.color = color;
-					newCreep.speed = Random.Range (1f - speedRandomness, 1f + speedRandomness) * CREEP_BASE_SPEED;
-					newCreep.damage = Random.Range (1f - damageRandomness, 1f + damageRandomness) * CREEP_BASE_DAMAGE;
-					newCreep.life = Random.Range (1f - lifeRandomness, 1f + lifeRandomness) * CREEP_BASE_LIFE;
-					newCreep.priorities = client.priorities;
-
-					creeps.Add (newCreep);
-				}
-			}
-
 			SpawnCreeps ();
 		} else {
-			Debug.LogError ("Trying to start an already started game!");
+			Debug.LogWarning ("Trying to start an already started game!");
 		}
 	}
 
 	public void SpawnCreeps() {
-		foreach (Creep creep in creeps) {
-			creep.transform.position = new Vector3(Random.Range(-xRange, xRange), Random.Range(-yRange, yRange), 0);
+
+		for (int t = 0; t < networkManager.connectionIds.Count; t++) {
+			Color color = colors [t];
+			int playerId = networkManager.connectionIds [t];
+			for (int c = 0; c < creepCountPerTeam; c++) {
+				Vector3 pos = new Vector3 (Random.Range (-xRange, xRange), Random.Range (-yRange, yRange), 0);
+				Creep newCreep = (Creep)GameObject.Instantiate (creepPrefab, pos, Quaternion.identity);
+				ClientData client = networkManager.GetClientData (playerId);
+				newCreep.transform.SetParent (GetCreepStore().transform);
+				newCreep.gc = this;
+				newCreep.creeps = creeps;
+				newCreep.gameObject.GetComponent<SpriteRenderer> ().color = color;
+				newCreep.team = t;
+				newCreep.color = color;
+				newCreep.speed = Random.Range (1f - speedRandomness, 1f + speedRandomness) * CREEP_BASE_SPEED;
+				newCreep.damage = Random.Range (1f - damageRandomness, 1f + damageRandomness) * CREEP_BASE_DAMAGE;
+				newCreep.life = Random.Range (1f - lifeRandomness, 1f + lifeRandomness) * CREEP_BASE_LIFE;
+				newCreep.priorities = client.priorities;
+
+				creeps.Add (newCreep);
+			}
 		}
 	}
 
